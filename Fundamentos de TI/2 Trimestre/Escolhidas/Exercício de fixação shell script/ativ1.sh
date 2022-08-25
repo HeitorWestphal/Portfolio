1474cd2bad7a99d5a7796fb2cad89e37a006126e
Heitor <102591958+HeitorWestphal@users.noreply.github.com>
2022-08-25 18:21:31 -0300

    Delete Fundamentos de TI directory

--- Fundamentos de TI/2 Trimestre/Escolhidas/Exercício de fixação shell script/ativ1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Bom dia! Qual é o seu nome?"
-read nome
-echo ""
-echo "Olá, $nome"
-
--- Fundamentos de TI/2 Trimestre/Escolhidas/Exercício de fixação shell script/ativ3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Digite o diretório cujos componentes deseja listar:"
-read dir
-echo ""
-echo "Os componentes deste diretório são:"
-ls -a $dir
